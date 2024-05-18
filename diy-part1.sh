1cee25179472bc47c4735a00f99bfe0146f23f57
nyrf <xiaojin0913@163.com>
2024-05-18 21:38:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,6 +33,6 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # svn export https://github.com/immortalwrt/packages/tree/openwrt-23.05/net/vlmcsd  package/vlmcsd
 # sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
 # ./scripts/feeds update -i && ./scripts/feeds install -a
-git clone https://github.com/m0eak/luci-app-turboacc-mtk package/luci-app-turboacc-mtk
-sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
-./scripts/feeds update -i && ./scripts/feeds install -a
+#git clone https://github.com/m0eak/luci-app-turboacc-mtk package/luci-app-turboacc-mtk
+#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' ./package/*/Makefile
+#./scripts/feeds update -i && ./scripts/feeds install -a
